366ed820bbdcef348a4d5d874c4e82eaa395a465
Nhan Nguyen <nguy0821@algonquinlive.com>
2020-06-02 22:10:09 -0400

    step 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         Button mbutton = findViewById(R.id.myButton);
         mbutton.setOnClickListener(v -> Toast.makeText(MainActivity.this
